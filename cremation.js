40a258697b0198e0ecd5a1809ca4eee2378a60ae
geekahedron <d_j_@hotmail.com>
2017-04-06 13:05:07 -0400

    cremation comment fixes

--- cremation.js
+++ cremation.js
@@ -9,7 +9,7 @@ requires:['Default dataset*'],
 sheets:{'cremationSheet':'https://github.com/geekahedron/heritage/blob/master/img/cremationModIconSheet.png?raw=true'},
 func:function()
 {
-	//First we create a couple new resources :
+	//Create new unit, ash, as a byproduct
 	new G.Res({
 		name:'ash',
 		desc:'[ash] is a byproduct of firemaking activities that can be used in agriculture.',
@@ -17,6 +17,7 @@ func:function()
 		category:'misc',
 	});
 	
+	// Crematorium as a new unit to do the work
 	new G.Unit({
 		name:'crematorium',
 		desc:'@processes goods with fire<>A [crematorium] is an efficient way to dispose of [corpse]s.',//TODO : desc
@@ -37,12 +38,12 @@ func:function()
 	});
 	
 	
-	//Then we add a new technology which is required by the artisans to gain access to the "hot sauce" mode :
+	//Add technology to unlock cremation mode
 	new G.Tech({
 		name:'cremation',
 		desc:'@[corpse]s can now be burned into [ash].//Cremation can save space and prevent the spread of disease..',
 		icon:[0,1,'cremationSheet'],
-		cost:{'insight':10},
+		cost:{'insight':5},
 		req:{'fire-making':true},
 	});
 }
