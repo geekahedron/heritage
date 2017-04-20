eaf27d877f4dda17cb7f8523e5b111c89bf6acc3
geekahedron <d_j_@hotmail.com>
2017-04-20 16:33:44 -0400

    fix manifest name typo

--- heritage.js
+++ heritage.js
@@ -1,9 +1,9 @@
-G.AddData({
+GM.AddData({
 name:'Heritage mod',
 author:'geekahedron',
 desc:'A collection of mods and improvements for NeverEnding Legacy.',
 engineVersion:1,
-manifest:'https://rawgit.com/geekahedron/heritage/master/heritageodManifest.js',
+manifest:'https://rawgit.com/geekahedron/heritage/master/heritageModManifest.js',
 requires:['Default dataset*'],
 sheets:{
 	'heritageSheet':'https://rawgit.com/geekahedron/heritage/master/img/heritageModIconSheet.png',
