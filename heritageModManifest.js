fe4ffc7a437ab489c1a2d1d6a3790179d2e856d1
geekahedron <d_j_@hotmail.com>
2017-04-21 11:32:58 -0400

    Updated stable CDN direct link to commit file in manifest

--- heritageModManifest.js
+++ heritageModManifest.js
@@ -2,7 +2,7 @@ G.DeclareManifest({
 name:'Heritage mod',
 updates:{
 	// CDN of stable image for publication
-	// 'Heritage mod*':'https://cdn.rawgit.com/geekahedron/heritage/2dd7ba4928781a50038beb7878904ccd3938af0e/heritage.js',
-	'Heritage mod*':'https://rawgit.com/geekahedron/heritage/master/heritage.js',
+	'Heritage mod*':'https://cdn.rawgit.com/geekahedron/heritage/f42c3f20183eac6bfc43a7a19536c0d394ec2655/heritage.js',
+//	'Heritage mod*':'https://rawgit.com/geekahedron/heritage/master/heritage.js',
 }
 });
