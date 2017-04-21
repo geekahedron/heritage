7aa03f615663bfad607529d3c9c6a71ff537b4ff
geekahedron <d_j_@hotmail.com>
2017-04-20 23:14:45 -0400

    Redirect manifest at development version of the mod

--- heritageModManifest.js
+++ heritageModManifest.js
@@ -2,6 +2,7 @@ G.DeclareManifest({
 name:'Heritage mod',
 updates:{
 	// CDN of stable image for publication
-	'Heritage mod*':'https://cdn.rawgit.com/geekahedron/heritage/2dd7ba4928781a50038beb7878904ccd3938af0e/heritage.js',
+	// 'Heritage mod*':'https://cdn.rawgit.com/geekahedron/heritage/2dd7ba4928781a50038beb7878904ccd3938af0e/heritage.js',
+	'Heritage mod*':'https://rawgit.com/geekahedron/heritage/master/heritage.js',
 }
 });
