3a5c2d8a3853f0432f25c2b6df3f536e29a768b5
geekahedron <d_j_@hotmail.com>
2017-04-29 20:43:30 -0400

    Updated name for master branch

--- heritageModManifest.js
+++ heritageModManifest.js
@@ -1,7 +1,7 @@
 G.DeclareManifest({
-name:'NeverEnding Heritage mod',
+name:'Heritage mod',
 updates:{
 	// CDN of stable image for publication
-	'*Heritage mod*':'https://cdn.rawgit.com/geekahedron/heritage/78e4089419e26a81bf850777e9dd9698eb6694de/heritage.js',
+	'Heritage mod*':'https://cdn.rawgit.com/geekahedron/heritage/78e4089419e26a81bf850777e9dd9698eb6694de/heritage.js',
 }
 });
